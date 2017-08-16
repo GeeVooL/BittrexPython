ecde6eb623a0b51edaf1d7a581b9e3ff28e10e1f
Michał Dziewulski <michal@dziewulski.pl>
2017-08-16 23:22:11 +0200

    Bittrex API v1.1 fully implemented and documented
    TODO: Test each method

--- bittrex.py
+++ bittrex.py
@@ -3,197 +3,355 @@ import datetime
 import hmac
 import hashlib
 
-from config import *
+# ===============================================
+# BITTREX API CONFIG
+# ===============================================
+
+# Bittrex API version string
+API_VERSION = 'v1.1'
+
+# Bittrex base URL string
+BITTREX_BASEURL = 'https://bittrex.com/api/' + API_VERSION
+
+# Bittrex public API base URL string
+PUBLIC_BASEURL = BITTREX_BASEURL + '/public'
+
+# Bittrex account API base URL string
+ACCOUNT_BASEURL = BITTREX_BASEURL + '/account'
+
+# Bittrex market API base URL string
+MARKET_BASEURL = BITTREX_BASEURL + '/market'
+
+# ===============================================
 
 class Bittrex(object):
-    '''Handling REST API requests to Bittrex'''
+    """Handling REST API requests to Bittrex"""
     def __init__(self, api_key, api_secret):
         self.api_key = api_key
         self.api_secret = api_secret
 
     def make_auth_request(self, uri, params=None):
-        # TODO: Change public requests to use make_request(...)
+        """Make authorised request to Bittrex API
+                
+        Parameters:
+        uri = full path to REST API,
+        params (optional) = request parameters
+
+        Return:
+        requests response object
+        """
+
         nonce = datetime.datetime.now().timestamp()
-        uri += '?apikey=%s&nonce=%s' % (self.api_key, nonce)
+        base_params = {'apikey': self.api_key, 'nonce': nonce}
         api_sign = hmac.new(self.api_secret.encode(), uri.encode(), hashlib.sha512).hexdigest()
         header_data = {'apisign': api_sign}
+        
         if params is not None:
-            r = requests.get(uri, headers=header_data, params=params)
-        else:    
-            r = requests.get(uri, headers=header_data)
+            base_params.update(params)
+        
+        r = requests.get(uri, headers=header_data, params=base_params)
         r.raise_for_status()
         return r
 
     def make_request(self, uri, params=None):
-        # TODO: Make public request without auth data
-        pass
+        """Make unauthorised request to Bittrex API
+        
+        Parameters:
+        uri = full path to REST API,
+        params (optional) = request parameters
 
-    @property
-    def public(self):
-        class Public(object): # self -> p_self
-            def get_markets(p_self):
-                '''Used to get the open and available trading markets at Bittrex along with other meta data.'''
+        Return:
+        requests response object
+        """
 
-                uri = PUBLIC_BASEURL + '/getmarkets'
-                data = self.make_request(uri)
-                return data.json()
+        r = requests.get(uri, params=params)
+        r.raise_for_status()
+        return r
 
-            def get_currencies(p_self):
-                '''Used to get all supported currencies at Bittrex along with other meta data.'''
+    # PUBLIC REQUESTS
+    def get_markets(self):
+        """Used to get the open and available trading markets at Bittrex along with other meta data.
+        
+        Return:
+        json structure
+        """
 
-                uri = PUBLIC_BASEURL + '/getcurrencies'
-                data = self.make_request(uri)
-                return data.json()
-            
-            def get_ticker(p_self, market):
-                '''Used to get the current tick values for a market.
-                Parameters:
-                market = market name string, ex. "BTC-ETH"'''
+        uri = PUBLIC_BASEURL + '/getmarkets'
+        data = self.make_request(uri)
+        return data.json()
 
-                uri = PUBLIC_BASEURL + '/getticker'
-                data = self.make_request(uri, params={'market': market})
-                return data.json()
-            
-            def get_market_summaries(p_self):
-                '''Used to get the last 24 hour summary of all active exchanges.'''
+    def get_currencies(self):
+        """Used to get all supported currencies at Bittrex along with other meta data.
+        
+        Return:
+        json structure
+        """
 
-                uri = PUBLIC_BASEURL + '/getmarketsummaries'
-                data = self.make_request(uri)
-                return data.json()
-
-            def get_market_summary(p_self, market):
-                '''Used to get the last 24 hour summary of all active exchanges.
-                Parameters:
-                market = market name string, ex. "BTC-ETH"'''
-
-                uri = PUBLIC_BASEURL + '/getmarketsummary'
-                data = self.make_request(uri, params={'market': market})
-                return data.json()
-
-            def get_order_book(p_self, market, orderbook_type):
-                '''Used to get retrieve the orderbook for a given market.
-                Parameters:
-                market = market name string, ex. "BTC-ETH",
-                orderbook_type = "required buy", "sell" or "both" as orderbook type'''
-
-                uri = PUBLIC_BASEURL + '/getorderbook'
-                data = self.make_request(uri, params={'market': market, 'type': orderbook_type})
-                return data.json()
-            
-            def get_market_history(p_self, market):
-                '''Used to retrieve the latest trades that have occured for a specific market.
-                Parameters:
-                market = market name string, ex. "BTC-ETH"'''
-
-                uri = PUBLIC_BASEURL + '/getmarkethistory'
-                data = self.make_request(uri, params={'market': market})
-                return data.json()
-
-        return Public()
+        uri = PUBLIC_BASEURL + '/getcurrencies'
+        data = self.make_request(uri)
+        return data.json()
     
-    @property
-    def account(self):
-        class Account(object): # self -> a_self
-            def get_balances(a_self):
-                '''Used to retrieve all balances from your account.'''
+    def get_ticker(self, market):
+        """Used to get the current tick values for a market.
+        
+        Parameters:
+        market = market name string, ex. BTC-ETH
+        
+        Return:
+        json structure
+        """
 
-                uri = ACCOUNT_BASEURL + '/getbalances'
-                data = self.make_request(uri)
-                return data.json()
-
-            def get_balance(a_self, currency):
-                '''Used to retrieve the balance from your account for a specific currency.
-                Parameters:
-                currency = currency name string, ex. "BTC"'''
-
-                uri = ACCOUNT_BASEURL + '/getbalance'
-                data = self.make_request(uri, params={'currency': currency})
-                return data.json()
-
-            def get_deposit_address(a_self, currency):
-                '''Used to retrieve or generate an address for a specific currency.
-                If one does not exist, the call will fail and return ADDRESS_GENERATING until one is available.
-                Parameters:
-                currency = currency name string, ex. "BTC"'''
-
-                uri = ACCOUNT_BASEURL + '/getdepositaddress'
-                data = self.make_request(uri, params={'currency': currency})
-                return data.json()
-
-            def withdraw(a_self, currency, quantity, address, payment_id=None):
-                '''Used to withdraw funds from your account. note: please account for txfee.
-                Parameters:
-                currency = currency name string, ex. "BTC",
-                quantity = quantity of coins to withdraw,
-                address = address of destination wallet,
-                payment_id (optional) = used for CryptoNotes/BitShareX/Nxt optional field (memo/paymentid)'''
-
-                uri = ACCOUNT_BASEURL + '/withdraw'
-                parameters = {'currency': currency, 'quantity': quantity, 'address': address}
-                if payment_id is not None:
-                    parameters['paymentid'] = payment_id
-                data = self.make_request(uri, params=parameters)
-                return data.json()
-
-            def get_order(a_self, uuid):
-                '''Used to retrieve a single order by uuid.
-                Parameters:
-                uuid = uuid string of a single order'''
-
-                uri = ACCOUNT_BASEURL + '/getorder'
-                data = self.make_request(uri, params={'uuid': uuid})
-                return data.json()
-
-            def get_order_history(a_self, market=None):
-                '''Used to retrieve your order history.
-                Parameters:
-                market (optional) = market name string, ex. "BTC-ETH"'''
-
-                uri = ACCOUNT_BASEURL + '/getorderhistory'
-                if market is not None:
-                    data = self.make_request(uri, params={'market': market})
-                else:
-                    data = self.make_request(uri)
-                return data.json()
-            
-            def get_withdrawal_history(a_self, currency=None):
-                '''Used to retrieve your withdrawal history.
-                Parameters:
-                currency (optional) = currency name string, ex. "BTC"'''
-
-                uri = ACCOUNT_BASEURL + '/getwithdrawalhistory'
-                if currency is not None:
-                    data = self.make_request(uri, params={'currency': currency})
-                else:
-                    data = self.make_request(uri)
-                return data.json()
-            
-            def get_deposit_history(a_self, currency=None):
-                '''Used to retrieve your deposit history.
-                Parameters:
-                currency (optional) = currency name string, ex. "BTC"'''
-
-                uri = ACCOUNT_BASEURL + '/getdeposithistory'
-                if currency is not None:
-                    data = self.make_request(uri, params={'currency': currency})
-                else:
-                    data = self.make_request(uri)
-                return data.json()
-
-        return Account()
+        uri = PUBLIC_BASEURL + '/getticker'
+        data = self.make_request(uri, params={'market': market})
+        return data.json()
     
-    @property
-    def market(self):
-        # TODO: Finish impelementation
-        class Market(object): # self -> m_self
-            def buy_limit(m_self, market, quantity, rate):
-                pass
-            
-            def sell_limit(m_self, market, quantity, rate):
-                pass
+    def get_market_summaries(self):
+        """Used to get the last 24 hour summary of all active exchanges.
+        
+        Return:
+        json structure
+        """
 
-            def cancel(m_self, uuid):
-                pass
+        uri = PUBLIC_BASEURL + '/getmarketsummaries'
+        data = self.make_request(uri)
+        return data.json()
 
-            def get_open_orders()
-        return Market()
+    def get_market_summary(self, market):
+        """Used to get the last 24 hour summary of all active exchanges.
+        
+        Parameters:
+        market = market name string, ex. BTC-ETH
+        
+        Return:
+        json structure
+        """
+
+        uri = PUBLIC_BASEURL + '/getmarketsummary'
+        data = self.make_request(uri, params={'market': market})
+        return data.json()
+
+    def get_order_book(self, market, orderbook_type):
+        """Used to get retrieve the orderbook for a given market.
+        
+        Parameters:
+        market = market name string, ex. BTC-ETH,
+        orderbook_type = "required buy", "sell" or "both" as orderbook type
+        
+        Return:
+        json structure
+        """
+
+        uri = PUBLIC_BASEURL + '/getorderbook'
+        data = self.make_request(uri, params={'market': market, 'type': orderbook_type})
+        return data.json()
+    
+    def get_market_history(self, market):
+        """Used to retrieve the latest trades that have occured for a specific market.
+        
+        Parameters:
+        market = market name string, ex. BTC-ETH
+        
+        Return:
+        json structure
+        """
+
+        uri = PUBLIC_BASEURL + '/getmarkethistory'
+        data = self.make_request(uri, params={'market': market})
+        return data.json()
+
+    # ACCOUNT REQUESTS
+    def get_balances(self):
+        """Used to retrieve all balances from your account.
+        
+        Return:
+        json structure
+        """
+
+        uri = ACCOUNT_BASEURL + '/getbalances'
+        data = self.make_auth_request(uri)
+        return data.json()
+
+    def get_balance(self, currency):
+        """Used to retrieve the balance from your account for a specific currency.
+        
+        Parameters:
+        currency = currency name string, ex. "BTC"
+        
+        Return:
+        json structure
+        """
+
+        uri = ACCOUNT_BASEURL + '/getbalance'
+        data = self.make_auth_request(uri, params={'currency': currency})
+        return data.json()
+
+    def get_deposit_address(self, currency):
+        """Used to retrieve or generate an address for a specific currency. If one does not exist, the call will fail and return ADDRESS_GENERATING until one is available.
+        
+        Parameters:
+        currency = currency name string, ex. "BTC"
+        
+        Return:
+        json structure
+        """
+
+        uri = ACCOUNT_BASEURL + '/getdepositaddress'
+        data = self.make_auth_request(uri, params={'currency': currency})
+        return data.json()
+
+    def withdraw(self, currency, quantity, address, payment_id=None):
+        """Used to withdraw funds from your account. note: please account for txfee.
+        
+        Parameters:
+        currency = currency name string, ex. "BTC",
+        quantity = quantity of coins to withdraw,
+        address = address of destination wallet,
+        payment_id (optional) = used for CryptoNotes/BitShareX/Nxt optional field (memo/paymentid)
+        
+        Return:
+        json structure
+        """
+
+        uri = ACCOUNT_BASEURL + '/withdraw'
+        parameters = {'currency': currency, 'quantity': quantity, 'address': address}
+        if payment_id is not None:
+            parameters['paymentid'] = payment_id
+        data = self.make_auth_request(uri, params=parameters)
+        return data.json()
+
+    def get_order(self, uuid):
+        """Used to retrieve a single order by uuid.
+        
+        Parameters:
+        uuid = uuid string of a single order
+        
+        Return:
+        json structure
+        """
+
+        uri = ACCOUNT_BASEURL + '/getorder'
+        data = self.make_auth_request(uri, params={'uuid': uuid})
+        return data.json()
+
+    def get_order_history(self, market=None):
+        """Used to retrieve your order history.
+        
+        Parameters:
+        market (optional) = market name string, ex. BTC-ETH
+        
+        Return:
+        json structure
+        """
+
+        uri = ACCOUNT_BASEURL + '/getorderhistory'
+        if market is not None:
+            data = self.make_auth_request(uri, params={'market': market})
+        else:
+            data = self.make_auth_request(uri)
+        return data.json()
+    
+    def get_withdrawal_history(self, currency=None):
+        """Used to retrieve your withdrawal history.
+        
+        Parameters:
+        currency (optional) = currency name string, ex. "BTC"
+        
+        Return:
+        json structure
+        """
+
+        uri = ACCOUNT_BASEURL + '/getwithdrawalhistory'
+        if currency is not None:
+            data = self.make_auth_request(uri, params={'currency': currency})
+        else:
+            data = self.make_auth_request(uri)
+        return data.json()
+    
+    def get_deposit_history(self, currency=None):
+        """Used to retrieve your deposit history.
+        
+        Parameters:
+        currency (optional) = currency name string, ex. "BTC"
+        
+        Return:
+        json structure
+        """
+
+        uri = ACCOUNT_BASEURL + '/getdeposithistory'
+        if currency is not None:
+            data = self.make_auth_request(uri, params={'currency': currency})
+        else:
+            data = self.make_auth_request(uri)
+        return data.json()
+
+    # MARKET REQUESTS
+    def buy_limit(self, market, quantity, rate):
+        """Used to place a buy order in a specific market. Use buylimit to place limit orders. Make sure you have the proper permissions set on your API keys for this call to work
+        
+        Parameters:
+        market = market name string, ex. BTC-ETH,
+        quantity = quantity of coins to withdraw,
+        rate = the rate at which to place the order.
+        
+        Return:
+        json structure
+        """
+
+        uri = ACCOUNT_BASEURL + '/buylimit'
+        parameters = {'market': market,
+                      'quantity': quantity,
+                      'rate': rate}
+
+        data = self.make_auth_request(uri, params=parameters)
+        return data.json()
+    
+    def sell_limit(self, market, quantity, rate):
+        """Used to place an sell order in a specific market. Use selllimit to place limit orders. Make sure you have the proper permissions set on your API keys for this call to work.
+        
+        Parameters:
+        market = market name string, ex. BTC-ETH,
+        quantity = quantity of coins to withdraw,
+        rate = the rate at which to place the order.
+        
+        Return:
+        json structure
+        """
+
+        uri = ACCOUNT_BASEURL + '/buylimit'
+        parameters = {'market': market,
+                      'quantity': quantity,
+                      'rate': rate}
+
+        data = self.make_auth_request(uri, params=parameters)
+        return data.json()
+
+    def cancel(self, uuid):
+        """Used to cancel a buy or sell order.
+        
+        Parameters:
+        uuid = uuid of buy or sell order
+        
+        Return:
+        json structure
+        """
+
+        uri = ACCOUNT_BASEURL + '/cancel'
+        data = self.make_auth_request(uri, params={'uuid': uuid})
+        return data.json()
+
+    def get_open_orders(self, market=None):
+        """Get all orders that you currently have opened. A specific market can be requested.
+        
+        Parameters:
+        market (optional) = market name string, ex. BTC-ETH
+        
+        Return:
+        json structure
+        """
+
+        uri = ACCOUNT_BASEURL + '/getopenorders'
+        if market is not None:
+            data = self.make_auth_request(uri, params={'currency': market})
+        else:
+            data = self.make_auth_request(uri)
+        return data.json()
